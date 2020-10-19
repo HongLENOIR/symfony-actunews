70a82dc565015f1c0181b289c79dbd1b82f5c77e
HongLENOIR <59973797+HongLENOIR@users.noreply.github.com>
2020-10-19 17:30:28 +0200

    premier commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     *  Page / Action : Accueil
+     */
+    public function index()
+    {
+        return new Response('<h1>Page Accueil</h1>');
+    }
+
+    /**
+     *  Page / Action : Contact
+     */
+
+    public function contact()
+    {
+        return new Response('<h1>Page Contact</h1>');
+    }
+
+    /**
+     *  Page / Action : Categorie
+     *  Permet d'afficher les articles d'une catégorie
+     * @Route("/{alias}", name="default_category", methods={"GET"})
+     * name de la route, convention: controller_action pour facilier trouver dans user controller!
+     */
+
+    public function category($alias)
+    {
+
+        return new Response("<h1>Page $alias</h1>");
+
+    }
+    /**
+     *  Page / Action : Article
+     *  Permet d'afficher un article du site
+     * @Route("/{category}/{alias}_{id}.html", name="default_article", methods={"GET"})
+     */
+    public function article()
+    {
+        # URL: https://localhost:8000/politique/couvre-feu-quand-la-situation-sanitaire-s-ameliorera-t-elle_14155614.html
+        #3 parametre: categorie, alias(le titre d'article), _id.html
+        return new Response("<h1>Page Article</h1>");
+    }
+
+}
+
+
